e56a53eb55e4354b7118392acb306fcbfff2dc13
iThinkyouknow <woontlt.dev@gmail.com>
2023-11-14 01:18:51 +0800

    fade in for first in search history

--- src/components/SearchHistory.jsx
+++ src/components/SearchHistory.jsx
@@ -5,7 +5,7 @@ export function SearchHistory({
 }) {
 
     return (
-        <li className='flex rounded justify-between rounded-2xl bg-1A1A1A/50 p-3 sm:p-6 pr-4 gap-4'>
+        <li className='flex rounded justify-between rounded-2xl bg-1A1A1A/50 p-3 sm:p-6 pr-4 gap-4 fade-in first:opacity-0'>
             <div className='flex flex-col sm:items-center sm:flex-row sm:justify-between sm:flex-1'>
                 <span className='font-bold sm:font-normal'>{name}</span>
                 <span className='text-white/50 text-2xs sm:text-base'>{formattedDateTime}</span>
